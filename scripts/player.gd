9e1f22565a61c2511642df151bf53c9003edaab1
Tofu-Gang <tofugangsw@gmail.com>
2026-09-10 00:29:36 +0200

    25. Specifying Data Types

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,13 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var my_age = 38
+	var my_age: int = 38
 	print("my_age = ", my_age)
 	my_age += 1
 	print("Happy Birthday! my_age = ", my_age)
-	var is_daytime = true
+	var is_daytime: bool = true
 	print("Is it daytime? ", is_daytime)
 	is_daytime = !is_daytime
 	print("How about now? ", is_daytime)
+	var name: String = "Tofu-Gang"
+	print("Who are you? ", name)
