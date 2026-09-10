e6a6a57cf417c3c8129b4da300183f2825c8a83c
Tofu-Gang <tofugangsw@gmail.com>
2026-09-10 23:55:35 +0200

    31. Function Return Values

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -4,7 +4,13 @@ extends RigidBody2D
 func _ready() -> void:
 	var my_vector: Vector2 = Vector2(-100.0, -50.0)
 	apply_impulse(my_vector)
-	my_function("Hello, world!")
+	var result: bool = my_function("Hello, world!")
+	print("result: ", result)
+	print("Subtraction: ", subtract(10.0, 5.0))
 
-func my_function(message: String) -> void:
+func my_function(message: String) -> bool:
 	print("Here's your message! ", message)
+	return true
+
+func subtract(operand_1: float, operand_2: float) -> float:
+	return operand_1 - operand_2
