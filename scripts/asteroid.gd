3f94b6d854d524263dfd043f711ea85f6c595858
Tofu-Gang <tofugangsw@gmail.com>
2026-09-11 00:29:10 +0200

    33. Process Function

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -5,13 +5,13 @@ func _ready() -> void:
 	const FORCE_RANGE: float = 200
 	apply_random_impulse(FORCE_RANGE)
 	
-func apply_random_impulse(range: float) -> void:
-	apply_impulse(get_random_force_vector(range))
+func apply_random_impulse(force_range: float) -> void:
+	apply_impulse(get_random_force_vector(force_range))
 
-func get_random_force_vector(range: float) -> Vector2:
-	var x: float = get_random_force(range)
-	var y: float = get_random_force(range)
+func get_random_force_vector(force_range: float) -> Vector2:
+	var x: float = get_random_force(force_range)
+	var y: float = get_random_force(force_range)
 	return Vector2(x, y)
 
-func get_random_force(range: float) -> float:
-	return randf_range(-range, range)
+func get_random_force(force_range: float) -> float:
+	return randf_range(-force_range, force_range)
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,3 +3,21 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+func _process(_delta: float) -> void:
+	pass
+	
+func _physics_process(_delta: float) -> void:
+	const FORCE_RANGE: float = 200
+	apply_random_force(FORCE_RANGE)
+
+func apply_random_force(force_range: float) -> void:
+	apply_force(get_random_force_vector(force_range))
+
+func get_random_force_vector(force_range: float) -> Vector2:
+	var x: float = get_random_force(force_range)
+	var y: float = get_random_force(force_range)
+	return Vector2(x, y)
+
+func get_random_force(force_range: float) -> float:
+	return randf_range(-force_range, force_range)
