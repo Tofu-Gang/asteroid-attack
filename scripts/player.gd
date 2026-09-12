9edafc3ff6f506e90fe489b49a3cc93477d1de35
Tofu Gang <tofugangsw@gmail.com>
2026-09-12 16:26:21 +0200

    36. Variable Scope

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,17 @@
 extends RigidBody2D
 
+const FORCE: float = 1500
+const FORCE_RIGHT: Vector2 = Vector2(FORCE, 0)
+const FORCE_LEFT: Vector2 = Vector2(-FORCE, 0)
+const FORCE_UP: Vector2 = Vector2(0, -FORCE)
+const FORCE_DOWN: Vector2 = Vector2(0, FORCE)
+
 func _physics_process(_delta: float) -> void:
-	const FORCE: float = 1500
-	
 	if Input.is_action_pressed("move_right"):
-		apply_force(Vector2(FORCE, 0))
+		apply_force(FORCE_RIGHT)
 	elif Input.is_action_pressed("move_left"):
-		apply_force(Vector2(-FORCE, 0))
+		apply_force(FORCE_LEFT)
 	elif Input.is_action_pressed("move_up"):
-		apply_force(Vector2(0, -FORCE))
+		apply_force(FORCE_UP)
 	elif Input.is_action_pressed("move_down"):
-		apply_force(Vector2(0, FORCE))
+		apply_force(FORCE_DOWN)
