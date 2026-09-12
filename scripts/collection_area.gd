fb3ac82e3e757c69136e290ef138d6585fd6b77e
Tofu Gang <tofugangsw@gmail.com>
2026-09-12 18:11:38 +0200

    42. Sounds & References

--- scripts/collection_area.gd
+++ scripts/collection_area.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
+@onready
+var collect_sound: AudioStreamPlayer2D = $CollectSound
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("asteroids"):
 		body.queue_free()
+		collect_sound.play()
 		print("Asteroid collected!")
 	else:
 		print("Not an asteroid, doing nothing.")
