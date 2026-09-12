2848267a798baa24e085e18bc7b1390ada3dd2e1
Tofu Gang <tofugangsw@gmail.com>
2026-09-12 17:55:03 +0200

    41. Groups & Freeing

--- scripts/collection_area.gd
+++ scripts/collection_area.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Body entered the collection area! ", body.name)
-
-func _on_body_exited(body: Node2D) -> void:
-	print("Body exited the collection area! ", body.name)
+	if body.is_in_group("asteroids"):
+		body.queue_free()
+		print("Asteroid collected!")
+	else:
+		print("Not an asteroid, doing nothing.")
+		 
